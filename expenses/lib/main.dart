3441de966cc5fb3b3932f53d15e5a774c09bccae
Luciano Contri <lucianoscontri@gmail.com>
2022-04-05 23:39:02 -0300

    primeira tela

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.orange,
+              child: Text('Gráfico'),
+              elevation: 3,
+            ),
+          ),
+          const Card(
+            child: Text('Lista de Transações'),
+          )
+        ],
+      ),
+    );
+  }
+}
